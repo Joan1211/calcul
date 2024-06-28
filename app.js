01b2ce58f367b0f2b052d296fd6c0d5ca74f742d
Joan1211 <59017905+Joan1211@users.noreply.github.com>
2024-06-28 06:58:41 +0200

    update

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// Prevent zooming
-document.addEventListener('touchmove', function (event) {
-    if (event.scale !== 1) {
-        event.preventDefault();
-    }
-}, { passive: false });
-
-let lastTouchEnd = 0;
-document.addEventListener('touchend', function (event) {
-    const now = (new Date()).getTime();
-    if (now - lastTouchEnd <= 300) {
-        event.preventDefault();
-    }
-    lastTouchEnd = now;
-}, false);
-
 // Initialize IndexedDB
 let db;
 const dbName = 'MentalMathTrainerDB';
